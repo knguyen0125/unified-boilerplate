45daf4eb2fc474573fa2b20c5229629acd8ead80
Kevin Nguyen <k.nguyen0125@gmail.com>
2024-03-24 14:58:21 +0700

    feat: oidc-provider esm

--- apps/backend/src/modules/auth/oidc-provider/models/index.ts
+++ apps/backend/src/modules/auth/oidc-provider/models/index.ts
@@ -1,17 +1,17 @@
-import Session  from './session.entity';
-import AccessToken  from './access-token.entity';
-import AuthorizationCode  from './authorization-code.entity';
-import RefreshToken  from './refresh-token.entity';
-import DeviceCode  from './device-code.entity';
-import ClientCredentials  from './client-credentials.entity';
-import Client  from './client.entity';
-import InitialAccessToken  from './initial-access-token.entity';
-import RegistrationAccessToken  from './registration-access-token.entity';
-import Interaction  from './interaction.entity';
-import ReplayDetection  from './replay-detection.entity';
-import PushAuthorizationRequest  from './push-authorization-request.entity';
-import Grant  from './grant.entity';
-import BackchannelAuthenticationRequest  from './backchannel-authentication-request.entity';
+import Session from './session.entity';
+import AccessToken from './access-token.entity';
+import AuthorizationCode from './authorization-code.entity';
+import RefreshToken from './refresh-token.entity';
+import DeviceCode from './device-code.entity';
+import ClientCredentials from './client-credentials.entity';
+import Client from './client.entity';
+import InitialAccessToken from './initial-access-token.entity';
+import RegistrationAccessToken from './registration-access-token.entity';
+import Interaction from './interaction.entity';
+import ReplayDetection from './replay-detection.entity';
+import PushAuthorizationRequest from './push-authorization-request.entity';
+import Grant from './grant.entity';
+import BackchannelAuthenticationRequest from './backchannel-authentication-request.entity';
 
 export const oidcModelMap = {
   Session: Session,
