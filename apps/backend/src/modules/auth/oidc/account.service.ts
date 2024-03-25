035ed33b6944db9d9e95bf103a334adda610f7a5
Kien Nguyen <kien.nguyen@your.rentals>
2024-03-25 16:46:33 +0700

    feat: implement impersonation

--- apps/backend/src/modules/auth/oidc/account.service.ts
+++ apps/backend/src/modules/auth/oidc/account.service.ts
@@ -8,7 +8,7 @@ export class AccountService {
     return {
       accountId: id,
       async claims(use, scope) {
-        console.log(use);
+        // These claims are for `use` of `id_token` or `userinfo`
         return {
           sub: id,
         };
@@ -16,6 +16,23 @@ export class AccountService {
     };
   };
   extraTokenClaims: Configuration['extraTokenClaims'] = async (ctx, token) => {
+    // Support for impersonation
+    if (token.kind === 'AccessToken') {
+      // Only "kien" can impersonate
+      // TODO: Make this an actual check
+      if (token.accountId && token.accountId === 'kien') {
+        if (
+          token?.claims &&
+          token?.claims['access_token'] &&
+          token?.claims['access_token']['act_as']
+        ) {
+          const actAs = token.claims['access_token']['act_as']['value'];
+          return {
+            act_as: actAs,
+          };
+        }
+      }
+    }
     return undefined;
   };
 }
